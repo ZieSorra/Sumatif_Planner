ea1206dfabba87289138efb9b5ff9db71ee35ca8
Classora <altearzie@gmail.com>
2026-09-10 12:23:55 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,17 @@
+// ==========================================
+// SUPABASE CLIENT
+// ==========================================
+
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    SUPABASE_ANON_KEY,
+    {
+        auth: {
+            persistSession: false,
+            autoRefreshToken: true,
+            detectSessionInUrl: true
+        }
+    }
 );
 
 console.log("Supabase client berhasil dibuat.");
