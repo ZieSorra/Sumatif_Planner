4bf2256397960812456373c183ba5e1b53f63125
Classora <altearzie@gmail.com>
2026-09-11 21:52:48 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://jizgwoavrxxpfzkwfbma.supabase.co";
+const SUPABASE_URL = "https://qkaqrnfiyngjvfckacdz.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_9eGSinz7ytpNZ2HblVgAxg_oy_HLONS";
