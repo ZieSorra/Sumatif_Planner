83184c7ff8d7c1511c3d07076b633da981f91bcb
Classora <altearzie@gmail.com>
2026-09-14 13:05:52 +0700

    Bump data master module cache version

--- js/schedule-board-fix.js
+++ js/schedule-board-fix.js
@@ -43,7 +43,7 @@
 (function bootMaster() {
     if (document.querySelector('script[data-master-data="1"]')) return;
     const script = document.createElement("script");
-    script.src = "js/master.js?v=1";
+    script.src = "js/master.js?v=2";
     script.dataset.masterData = "1";
     script.onload = () => console.log("[Master] Modul Data Master dimuat.");
     script.onerror = error => console.error("[Master] Gagal memuat Data Master:", error);
